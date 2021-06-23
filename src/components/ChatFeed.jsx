fd11f7f66ccd781b8a37089f8aa0594ba1d5f511
Zarema <zaremakayasova@gmail.com>
2021-06-23 18:02:28 -0400

    built MessageForm component, modified ChatFeed and TheirMessage components

--- src/components/ChatFeed.jsx
+++ src/components/ChatFeed.jsx
@@ -7,6 +7,19 @@ const ChatFeed = props => {
     const { chats, activeChat, userName, messages } = props;
     const chat = chats && chats[activeChat];
 
+    const renderReadReceipts = (message, isMyMessage) => {
+        return chat.people.map((person, index) => person.last_read === message.id && (
+            <div
+                key={`read_${index}`}
+                className='read-receipt'
+                style={{
+                    float: isMyMessage ? 'right' : 'left',
+                    backgroundImage: `url(${person?.person?.avatar})`
+                }}
+            />
+        ))
+    }
+
     const renderMessages = () => {
         const keys = Object.keys(messages);
 
@@ -25,7 +38,9 @@ const ChatFeed = props => {
                         }
                     </div >
                     <div className='read-receipts' style={{ marginRight: isMyMessage ? '18px' : '0px', marginLeft: isMyMessage ? '0px' : '68px' }}>
-                        read-receipts
+                        {
+                            renderReadReceipts(message, isMyMessage)
+                        }
                     </div>
                 </div>
             );
@@ -40,7 +55,9 @@ const ChatFeed = props => {
             <div className='chat-title-container'>
                 <div className='chat-title'>{chat.title}</div>
                 <div className='chat-subtitle'>
-                    {chat.people.map(person => '${person.person.username}')}
+                    {
+                        chat.people.map((person) => `${person.person.username} `)
+                    }
                 </div>
             </div>
             {renderMessages()}
